b683a93ac7ba8acbefb6f16cbbf00e99174aff4c
arin n dinda <arindachusnul3@gmail.com>
2026-05-18 11:28:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
+st.write(pehitungan COD dan perbandingan dengan baku mutu)
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
